131f175c439c8fc652e6be60c1ebceeef4b53d12
Leoni Sandes <leonisandes@gmail.com>
2019-08-30 15:47:07 +1000

    Organize configurations

--- index.js
+++ index.js
@@ -3,24 +3,33 @@ const express = require('express');
 const parser = require('body-parser');
 const dotenv = require('dotenv-safe');
 
+// Dotenv
 dotenv.config({
     allowEmptyValues: true,
-    path: process.env.NODE_ENV ? '.env' : '.env.dev',
+    path: isProductionMode() ? '.env' : '.env.dev',
 });
 
-if (process.env.NODE_ENV) {
-    console.log('Executando em modo produção.');
+if (isProductionMode()) {
+    console.log('Executing in production mode.');
 } else {
-    console.log('Executando em modo desenvolvimento.');
+    console.log('Executing in development mode.');
 }
+//
 
-const hostname = '127.0.0.1';
-const port = 8080;
-
+// Express
 const app = express();
 app.use(parser.json());
+//
 
+// Http
+const hostname = process.env.SERVER_HOST;
+const port = process.env.SERVER_PORT;
 http.createServer(app)
     .listen(port, hostname, () => {
         console.log(`Server running at http://${hostname}:${port}`);
     });
+//
+
+// Helpers functions
+function isProductionMode() { return process.env.NODE_ENV === 'PROD'; }
+//
