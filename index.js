fbf926d59f77639f91bf5253384e44047b0f460e
Leoni Sandes <leonisandes@gmail.com>
2019-08-30 15:34:26 +1000

    Add env config

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const http = require('http');
 const express = require('express');
 const parser = require('body-parser');
+const dotenv = require('dotenv-safe');
+
+dotenv.config({
+    allowEmptyValues: true,
+    path: process.env.NODE_ENV ? '.env' : '.env.dev',
+});
+
+if (process.env.NODE_ENV) {
+    console.log('Executando em modo produção.');
+} else {
+    console.log('Executando em modo desenvolvimento.');
+}
 
 const hostname = '127.0.0.1';
 const port = 8080;
