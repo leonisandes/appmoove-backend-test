82b19c361fd6650a8b8b3814309008709090f041
Leoni Sandes <leonisandes@gmail.com>
2019-09-07 12:01:31 +1000

    Fixing adicionar produto to only return message and status 201

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -1,9 +1,11 @@
+const MenssagemDTO = require('../dtos/mensagemDTO');
+
 module.exports = app => ({
     adicionar: async (req, res) => {
         app.commons.validation.hasErrors(req);
 
-        const produtoCriado = await app.services.produtoService.adicionar(req.body);
-        res.status(201).send(produtoCriado);
+        await app.services.produtoService.adicionar(req.body);
+        res.status(201).send(new MenssagemDTO('Produto cadastrado'));
     },
     listar: async (req, res) => {
         const produtos = await app.services.produtoService.listar();
