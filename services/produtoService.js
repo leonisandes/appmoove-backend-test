295e6313a5ac247dc413cd8e3d9b2b3dbf63212f
Leoni Sandes <leonisandes@gmail.com>
2019-09-08 11:05:29 +1000

    Removing try catchs and refactory the code

--- services/produtoService.js
+++ services/produtoService.js
@@ -1,34 +1,15 @@
 const ProdutoDTO = require('../dtos/produtoDTO');
 const DetalheProdutoDTO = require('../dtos/detalheProdutoDTO');
 
-function lancaOErroELoga(e) {
-    console.error(`Ocorreu um erro ao adicionar produto: ${e}`);
-    // TODO: Ajustar mensagem igual a documentação
-    throw Error('Algum problema inesperado aconteceu, não se preocupe estamos atuando...');
-}
-// TODO: refactory criar um wrap para tratar a mensagem de erro
+const wrap = fn => (...args) => fn(...args).catch(e => {
+    console.error(`Ocorreu um erro desconhecido: ${e}`);
+    throw Error('Ocorreu um erro desconhecido.');
+});
+
 module.exports = app => ({
-    adicionar: async produto => {
-        try {
-            return await app.repositories.produtoRepository.adicionar(produto);
-        } catch (e) {
-            throw lancaOErroELoga(e);
-        }
-    },
-    listar: async () => {
-        try {
-            return await app.repositories.produtoRepository.listar()
-                .then(produtos => produtos.map(p => new ProdutoDTO(p.nome, p.valor_unitario, p.qtde_estoque)));
-        } catch (e) {
-            throw lancaOErroELoga(e);
-        }
-    },
-    detalhe: async id => {
-        try {
-            return await app.repositories.produtoRepository.detalhe(id)
-                .then(detalhe => new DetalheProdutoDTO(detalhe.nome, detalhe.valor_unitario, detalhe.qtde_estoque, detalhe.valorUltimaVenda));
-        } catch (e) {
-            throw lancaOErroELoga(e);
-        }
-    },
+    adicionar: wrap(async produto => app.repositories.produtoRepository.adicionar(produto)),
+    listar: wrap(async () => app.repositories.produtoRepository.listar()
+        .then(produtos => produtos.map(p => new ProdutoDTO(p.nome, p.valor_unitario, p.qtde_estoque)))),
+    detalhe: wrap(async id => app.repositories.produtoRepository.detalhe(id)
+        .then(detalhe => new DetalheProdutoDTO(detalhe.nome, detalhe.valor_unitario, detalhe.qtde_estoque, detalhe.valorUltimaVenda))),
 });
