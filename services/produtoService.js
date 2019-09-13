1ab771f04003d23ca16c8de6b3ab2cb9b31577f5
Leoni Sandes <leonisandes@gmail.com>
2019-09-13 16:17:21 +1000

    Refactoring functions adicionar, listar, detalhe e adding function baixarEstoque

--- services/produtoService.js
+++ services/produtoService.js
@@ -1,15 +1,23 @@
 const ProdutoDTO = require('../dtos/produtoDTO');
 const DetalheProdutoDTO = require('../dtos/detalheProdutoDTO');
-
-const wrap = fn => (...args) => fn(...args).catch(e => {
-    console.error(`Ocorreu um erro desconhecido: ${e}`);
-    throw Error('Ocorreu um erro desconhecido.');
-});
+const SemEstoqueError = require('../errors/semEstoqueError');
 
 module.exports = app => ({
-    adicionar: wrap(async produto => app.repositories.produtoRepository.adicionar(produto)),
-    listar: wrap(async () => app.repositories.produtoRepository.listar()
+    adicionar: app.commons.wrap.handlerExceptionService(async produto => app.repositories.produtoRepository.adicionar(produto)),
+    listar: app.commons.wrap.handlerExceptionService(async () => app.repositories.produtoRepository.listar()
         .then(produtos => produtos.map(p => new ProdutoDTO(p.nome, p.valor_unitario, p.qtde_estoque)))),
-    detalhe: wrap(async id => app.repositories.produtoRepository.detalhe(id)
+    detalhe: app.commons.wrap.handlerExceptionService(async id => app.repositories.produtoRepository.detalhe(id)
         .then(detalhe => new DetalheProdutoDTO(detalhe.nome, detalhe.valor_unitario, detalhe.qtde_estoque, detalhe.valorUltimaVenda))),
+    baixarEstoque: app.commons.wrap.handlerExceptionService(async (id, quantidade) => {
+        app.repositories.produtoRepository.buscar(id).then(produto => {
+            const produtoClone = produto;
+            if (produtoClone.qtde_estoque >= quantidade) {
+                produtoClone.qtde_estoque -= quantidade;
+                console.log(`Produto clone: ${JSON.stringify(produtoClone)}`);
+                app.repositories.produtoRepository.atualizar(produtoClone);
+            } else {
+                throw new SemEstoqueError();
+            }
+        });
+    }),
 });
