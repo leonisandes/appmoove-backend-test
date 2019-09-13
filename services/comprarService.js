96bfb295325ac134d79c7a7f0998c78857069682
Leoni Sandes <leonisandes@gmail.com>
2019-09-13 18:03:57 +1000

    Adjusting compraService to use Pagamento error and refactory validation

--- services/comprarService.js
+++ services/comprarService.js
@@ -1,18 +1,18 @@
-const CompraError = require('../errors/compraError');
+const PagamentoError = require('../errors/pagamentoError');
 const TransacaoDTO = require('../dtos/transacaoDTO');
 
 module.exports = app => ({
-    comprar: app.commons.wrap.handlerExceptionService(async compra => {
-        app.clients.gatewayClient.debitar(compra.cartao).then(estado => {
-            app.services.produtoService.detalhe(compra.produto_id).then(produto => {
-                const valorTotal = produto.valor_unitario * compra.qtde_comprada;
-                app.services.transacaoService.adicionar(new TransacaoDTO(compra.produto_id, valorTotal, estado));
-            });
-            if (estado === 'APROVADO') {
-                app.services.produtoService.baixarEstoque(compra.produto_id, compra.qtde_comprada);
-            } else {
-                throw new CompraError(estado);
-            }
-        });
-    }),
+    comprar: app.commons.wrap.handlerExceptionService(async compra =>
+        app.clients.gatewayClient.debitar(compra.cartao)
+            .then(estado => {
+                app.services.produtoService.detalhe(compra.produto_id).then(produto => {
+                    const valorTotal = produto.valor_unitario * compra.qtde_comprada;
+                    app.services.transacaoService.adicionar(new TransacaoDTO(compra.produto_id, valorTotal, estado));
+                });
+                if (estado === 'APROVADO') {
+                    app.services.produtoService.baixarEstoque(compra.produto_id, compra.qtde_comprada);
+                } else {
+                    throw new PagamentoError(estado);
+                }
+            })),
 });
